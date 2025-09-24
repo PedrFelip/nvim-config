2fecf6c4722cbddac42e4157f4939c541e93aa23
Pedro Felipe <pfsilva190406@gmail.com>
2025-09-24 12:47:55 -0300

    melhorando o atalhos, pra trabalhar melhor com buffers do neovim

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -26,7 +26,7 @@ return {
       lspconfig.pyright.setup({
         capabilities = capabilities
       })
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
       lspconfig.solargraph.setup({
