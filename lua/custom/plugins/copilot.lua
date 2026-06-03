b795ec6ae30037dc4f8c6515808a3705e99ebcc4
Pedro Felipe <pfsilva190406@gmail.com>
2026-06-03 14:28:39 -0300

    add como provider o copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,13 +1,15 @@
-vim.pack.add { 'https://github.com/zbirenbaum/copilot.lua' }
+vim.pack.add {
+  'https://github.com/zbirenbaum/copilot.lua',
+  'https://github.com/giuxtaposition/blink-cmp-copilot',
+}
 
 require('copilot').setup {
+  suggestion = { enabled = false },
+  panel = { enabled = false },
   filetypes = {
     markdown = false,
     help = false,
     gitcommit = false,
     gitrebase = false,
-  },
-  blink = {
-    enabled = true,
   },
 }
